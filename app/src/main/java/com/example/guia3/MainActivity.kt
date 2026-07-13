5d6feca5dc0387642790b1392525aaf6a929cf9a
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-07-13 12:57:52 -0600

    Lyfe Cycle by Logcat
    Revisando el ciclo de la activity y sos estados

--- app/src/main/java/com/example/guia3/MainActivity.kt
+++ app/src/main/java/com/example/guia3/MainActivity.kt
@@ -1,20 +1,58 @@
+
 package com.example.guia3
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        lifecycle.addObserver(MyLifeCycleObserver())
+        mostrarToast("onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mostrarToast("onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mostrarToast("onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mostrarToast("onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mostrarToast("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        mostrarToast("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarToast("onDestroy")
+    }
+
+    private fun mostrarToast(
+        mensaje: String,
+        duracion: Int = Toast.LENGTH_SHORT
+    ) {
+        Toast.makeText(
+            this,
+            "MainActivity -> $mensaje",
+            duracion
+        ).show()
     }
 }
