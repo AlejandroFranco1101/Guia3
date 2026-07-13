191e90db4953ed80cb6b6e47749be6e764cf9ee9
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-07-13 13:44:56 -0600

    Agregando Contador
    Cantador

--- app/src/main/java/com/example/guia3/MainActivity.kt
+++ app/src/main/java/com/example/guia3/MainActivity.kt
@@ -4,20 +4,30 @@ package com.example.guia3
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnAbrir: Button
+    private lateinit var btnIncrementar: Button
+    private lateinit var tvContador: TextView
+    private var contador: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnAbrir = findViewById(R.id.btnAbrir)
+        btnIncrementar = findViewById(R.id.btnIncrementar)
+        tvContador = findViewById(R.id.tvContador)
         lifecycle.addObserver(MyLifeCycleObserver("MainActivity"))
         btnAbrir.setOnClickListener{
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
+        }
+        btnIncrementar.setOnClickListener{
+            contador++
+            tvContador.text = "Valor contador: $contador"
         }
 
         mostrarToast("onCreate")
