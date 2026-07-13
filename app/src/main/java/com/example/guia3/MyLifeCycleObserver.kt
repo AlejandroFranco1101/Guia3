6e02410f05d9fe223dfda510b2f7c7bc655a885d
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-07-13 12:30:06 -0600

    Kicking off the Project
    Inicializando + primera clase.kt

---
+++ app/src/main/java/com/example/guia3/MyLifeCycleObserver.kt
@@ -0,0 +1,31 @@
+package com.example.guia3
+
+import android.util.Log
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+class MyLifeCycleObserver : DefaultLifecycleObserver {
+    override fun onCreate(owner: LifecycleOwner) {
+        super.onCreate(owner)
+        Log.d("MyLifeCycleObserver", "onCreate")
+    }
+    override fun onStart(owner: LifecycleOwner) {
+        super.onStart(owner)
+        Log.d("MyLifeCycleObserver", "onStart")
+    }
+    override fun onPause(owner: LifecycleOwner) {
+        super.onPause(owner)
+        Log.d("MyLifeCycleObserver", "onPause")
+    }
+    override fun onResume(owner: LifecycleOwner) {
+        super.onResume(owner)
+        Log.d("MyLifeCycleObserver", "onResume")
+    }
+    override fun onStop(owner: LifecycleOwner) {
+        super.onStop(owner)
+        Log.d("MyLifeCycleObserver", "onStop")
+    }
+    override fun onDestroy(owner: LifecycleOwner) {
+        super.onDestroy(owner)
+        Log.d("MyLifeCycleObserver", "onDestroy")
+    }
+}
