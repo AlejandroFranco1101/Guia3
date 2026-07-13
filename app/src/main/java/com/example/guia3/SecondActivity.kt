65cec238611e59092f55d621bfc640b40d2117d4
AlejandroFranco1101 <alejandrofranco1101@gmail.com>
2026-07-13 13:08:13 -0600

    Coding SecondActivity
    Se modifica el codigo

--- app/src/main/java/com/example/guia3/SecondActivity.kt
+++ app/src/main/java/com/example/guia3/SecondActivity.kt
@@ -1,20 +1,59 @@
 package com.example.guia3
-
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-
 class SecondActivity : AppCompatActivity() {
+    lateinit var btnRegresar: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // 1. Referencia al archivo layout
         setContentView(R.layout.activity_second)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        // 2. Referencia al boton
+        btnRegresar = findViewById(R.id.btnRegresar)
+        // 3. Registro del observador
+        lifecycle.addObserver(MyLifeCycleObserver("SecondActivity"))
+        // 4. Listener del boton
+        btnRegresar.setOnClickListener{
+            // 5. Uso de un intent explicito para iniciar una nueva Activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
+        mostrarToast("onCreate")
+    }
+    override fun onStart() {
+        super.onStart()
+        mostrarToast("onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        mostrarToast("onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        mostrarToast("onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        mostrarToast("onStop")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        mostrarToast("onRestart")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mostrarToast("onDestroy")
+    }
+    private fun mostrarToast(
+        mensaje: String,
+        duracion: Int = Toast.LENGTH_SHORT
+    ) {
+        Toast.makeText(
+            this,
+            "SecondActivity -> $mensaje",
+            duracion
+        ).show()
     }
 }
